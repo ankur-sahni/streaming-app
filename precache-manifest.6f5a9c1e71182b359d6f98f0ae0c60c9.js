2feb82e26197e58f0d021e574d905a54ab5a4608
Ankur Sahni <ankur.sahni.tech@gmail.com>
2020-10-17 18:45:00 +0530

    Updates

---
+++ precache-manifest.6f5a9c1e71182b359d6f98f0ae0c60c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9500706e6e341c6fbe3e912368713fd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b2d29863e8599a9363",
+    "url": "/static/css/2.7995f3ed.chunk.css"
+  },
+  {
+    "revision": "5681605bff1b4c15ea09",
+    "url": "/static/css/main.51759b4a.chunk.css"
+  },
+  {
+    "revision": "90b2d29863e8599a9363",
+    "url": "/static/js/2.b931cce0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b931cce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5681605bff1b4c15ea09",
+    "url": "/static/js/main.cd79d551.chunk.js"
+  },
+  {
+    "revision": "4e6e91f0606716916ff5",
+    "url": "/static/js/runtime-main.c8540731.js"
+  }
+]);
